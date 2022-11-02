1f4658a7d0a1b00c32e31276fdb5836f7dc13ff4
Alisson <alissoncorsair@hotmail.com>
2022-11-02 18:32:59 -0300

    controllers + user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { userRouter } from "./user/userRoutes";
 
 export const router = Router();
 
+router.use('/user', userRouter);
 router.get('/', (req, res) => {
     res.json({ message: 'Hello World' });
 });
