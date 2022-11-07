2cbc8130ec4a9be02ee57e769a2d2cd5ef938f02
Alisson <alissoncorsair@hotmail.com>
2022-11-07 20:23:25 -0300

    fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import Mailer from "../controllers/Mailer";
 import { userRouter } from "./user/userRoutes";
 
 export const router = Router();
 
 router.use('/user', userRouter);
-router.post('/email', Mailer.sendEmail);
 router.get('/', (req, res) => {
     res.json({ message: 'Hello World' });
 });
